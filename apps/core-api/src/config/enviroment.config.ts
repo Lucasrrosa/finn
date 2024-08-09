f128c4a95bb316bf741c0133963414761d488f59
Lucas Rosa <l_r_rosa@hotmail.com>
2024-08-09 15:42:24 -0300

    feat:removing core api

--- apps/core-api/src/config/enviroment.config.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config'
-
-export type EnviromentConfigType = {
-    nodenv: string
-}
-
-export default registerAs('enviroment', () => ({
-    nodenv: process.env.NODENV,
-}))
